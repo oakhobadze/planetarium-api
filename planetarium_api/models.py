4613237de099f9b7f14da551e31a6b2ce68bd430
Oleksandr Akhobadze <sandoahobadze@gmail.com>
2025-03-10 18:50:36 +0100

    creating basic structure of the project, implementing models and admin settings for the project

---
+++ planetarium_api/models.py
@@ -0,0 +1,54 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Reservation(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.created_at)
+
+
+class Ticket(models.Model):
+    row = models.IntegerField()
+    seat = models.IntegerField()
+    show_session = models.ForeignKey("ShowSession", on_delete=models.CASCADE, related_name="tickets")
+    reservation = models.ForeignKey("Reservation", on_delete=models.CASCADE, related_name="reserved_tickets")
+
+    def __str__(self):
+        return f"{self.row}-{self.seat}-{self.reservation}"
+
+
+class ShowSession(models.Model):
+    astronomy_show = models.ForeignKey("AstronomyShow", on_delete=models.CASCADE)
+    planetarium_dome = models.ForeignKey("PlanetariumDome", on_delete=models.CASCADE)
+    show_time = models.DateTimeField()
+
+    def __str__(self):
+        return str(self.show_time)
+
+
+class PlanetariumDome(models.Model):
+    name = models.CharField(max_length=100)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class ShowTime(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class AstronomyShow(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    show_times = models.ManyToManyField(ShowTime, related_name="astronomy_shows")
+
+    def __str__(self):
+        return self.title
