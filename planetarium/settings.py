3b7785d723beec01c130b3d912a9babd65e07af4
Oleksandr Akhobadze <sandoahobadze@gmail.com>
2025-03-12 18:44:31 +0100

    creating requirements.txt file and saving SECRET_KEY into .env

--- planetarium/settings.py
+++ planetarium/settings.py
@@ -9,9 +9,12 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
+
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c(6&rm2a!$a_xwap55d42exsh9&9sei1y4m30jkpde7)%2tk5y'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
