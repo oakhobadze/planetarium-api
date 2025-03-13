575dfef5cb988b02ae906753433d127d7f804eca
Oleksandr Akhobadze <sandoahobadze@gmail.com>
2025-03-13 23:08:28 +0100

    creating throttling

--- planetarium/settings.py
+++ planetarium/settings.py
@@ -130,7 +130,16 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-   "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_THROTTLE_CLASSES": [
+       "rest_framework.throttling.AnonRateThrottle",
+       "rest_framework.throttling.UserRateThrottle"
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+       "anon": "100/day",
+       "user": "1000/day",
+       "admin": "100000/day"
+    }
 }
 SPECTACULAR_SETTINGS = {
     'TITLE': 'Planetarium API Documentation',
