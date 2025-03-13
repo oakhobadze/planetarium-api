64229e41d3ba3034515e32e81019ce9b57f8fd54
Oleksandr Akhobadze <sandoahobadze@gmail.com>
2025-03-13 22:55:43 +0100

    creating documentation for the project

--- planetarium/urls.py
+++ planetarium/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/planetarium/", include("planetarium_api.urls", namespace="planetarium")),
+    path("api/doc/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 ]
